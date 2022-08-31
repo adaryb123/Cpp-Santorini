bb01a46e9830274020a0b253d8326c2b7b237a91
Adam Rybansky <adam.rybansky@centrum.sk>
2022-08-31 23:46:14 +0200

    can show available moves but without builds

--- santorini.cpp
+++ santorini.cpp
@@ -19,15 +19,21 @@ int main()
     testBoard.game_board[2][4].layer=2;
     testBoard.game_board[1][1].layer=1;
     testBoard.game_board[4][1].layer=3;
-    testBoard.game_board[4][1].black_present = true;
-    testBoard.game_board[3][3].white_present = true;
+    // testBoard.game_board[4][1].black_present = true;
+    // testBoard.game_board[3][3].white_present = true;
     testBoard.game_board[3][4].layer=3;
     testBoard.game_board[3][4].dome = true;
 
     std::cout << testBoard;
 
-    Move testMove {2,2,3,3,2,3};
-    std::cout << testMove;
+    std::vector<Move> testMoves;
+    get_available_moves(testBoard.game_board,'b',testMoves);
+    for (size_t i = 0 ; i < testMoves.size() ; i++){
+        std::cout << testMoves[i] << "\n";
+    }
+
+    // Move testMove {2,2,3,3,2,3};
+    // std::cout << testMove;
 
 	// std::vector<std::vector<char>> game_board;
 
